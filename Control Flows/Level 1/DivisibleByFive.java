d8e61da3899819db823d278ae4b560a1af50d031
khushi bharti <khushibharti845@gmail.com>
2025-08-19 16:58:25 +0530

    Create DivisibleByFive.java

---
+++ Control Flows/Level 1/DivisibleByFive.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DivisibleByFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // Checking divisibility
+        if (number % 5 == 0) {
+            System.out.println("Is the number " + number + " divisible by 5? Yes");
+        } else {
+            System.out.println("Is the number " + number + " divisible by 5? No");
+        }
+
+        sc.close();
+    }
+}
