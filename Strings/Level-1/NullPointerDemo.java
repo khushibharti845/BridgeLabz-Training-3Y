b9bbb3cd23916b5e9406818ac56377190784fcaf
Your Name <khushibharti845@gmail.com>
2025-09-05 11:37:59 +0530

    added

---
+++ Strings/Level-1/NullPointerDemo.java
@@ -0,0 +1,21 @@
+package String.Level1;
+class NullPointerDemo {
+    static void generateException() {
+        String text = null;
+        System.out.println(text.length());
+    }
+
+    static void handleException() {
+        String text = null;
+        try {
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException handled: " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        generateException();
+        handleException();
+    }
+}
