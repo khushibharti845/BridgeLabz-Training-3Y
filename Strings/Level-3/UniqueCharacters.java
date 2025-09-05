2ec0e8ea251077eb242ab63e910a10699e736189
Your Name <khushibharti845@gmail.com>
2025-09-05 14:58:32 +0530

    added

---
+++ Strings/Level-3/UniqueCharacters.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class UniqueCharacters {
+
+    public static int getLength(String text) {
+        int count = 0;
+        try {
+            while (true) {
+                text.charAt(count);
+                count++;
+            }
+        } catch (Exception e) {
+        }
+        return count;
+    }
+
+    public static char[] findUniqueChars(String text) {
+        int len = getLength(text);
+        char[] temp = new char[len];
+        int index = 0;
+
+        for (int i = 0; i < len; i++) {
+            char current = text.charAt(i);
+            boolean isUnique = true;
+            for (int j = 0; j < i; j++) {
+                if (text.charAt(j) == current) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                temp[index++] = current;
+            }
+        }
+
+        char[] result = new char[index];
+        for (int i = 0; i < index; i++) {
+            result[i] = temp[i];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = sc.next();
+
+        char[] unique = findUniqueChars(text);
+        System.out.print("Unique characters: ");
+        for (char c : unique) {
+            System.out.print(c + " ");
+        }
+        sc.close();
+    }
+}
