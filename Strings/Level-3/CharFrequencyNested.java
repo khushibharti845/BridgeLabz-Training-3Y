9861f265db6631c1c485c93529dbfddf2d4d0589
Your Name <khushibharti845@gmail.com>
2025-09-05 14:56:14 +0530

    added

---
+++ Strings/Level-3/CharFrequencyNested.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class CharFrequencyNested {
+
+    public static String[] findFrequency(String text) {
+        char[] chars = text.toCharArray();
+        int[] freq = new int[chars.length];
+
+        for (int i = 0; i < chars.length; i++) {
+            freq[i] = 1;
+            for (int j = i + 1; j < chars.length; j++) {
+                if (chars[i] == chars[j] && chars[i] != '0') {
+                    freq[i]++;
+                    chars[j] = '0';
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != '0') {
+                count++;
+            }
+        }
+
+        String[] result = new String[count];
+        int index = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != '0') {
+                result[index++] = chars[i] + " -> " + freq[i];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+
+        String[] freqData = findFrequency(text);
+
+        System.out.println("Character Frequencies:");
+        for (String s : freqData) {
+            System.out.println(s);
+        }
+    }
+}
