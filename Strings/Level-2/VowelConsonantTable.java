8ff543e77a90adfc415ad5f161b2658601e34e95
Your Name <khushibharti845@gmail.com>
2025-09-05 14:32:15 +0530

    added

---
+++ Strings/Level-2/VowelConsonantTable.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class VowelConsonantTable {
+
+    public static String checkCharType(char ch) {
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = (char)(ch + 32);
+        }
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return "Vowel";
+            } else {
+                return "Consonant";
+            }
+        }
+        return "Not a Letter";
+    }
+
+    public static String[][] analyzeString(String str) {
+        int length = 0;
+        try {
+            while (true) {
+                str.charAt(length);
+                length++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+        }
+        String[][] result = new String[length][2];
+        for (int i = 0; i < length; i++) {
+            char ch = str.charAt(i);
+            result[i][0] = String.valueOf(ch);
+            result[i][1] = checkCharType(ch);
+        }
+        return result;
+    }
+
+    public static void displayTable(String[][] table) {
+        System.out.println("Character\tType");
+        System.out.println("-------------------------");
+        for (int i = 0; i < table.length; i++) {
+            System.out.println(table[i][0] + "\t\t" + table[i][1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        String[][] table = analyzeString(input);
+
+        displayTable(table);
+
+        sc.close();
+    }
+}
