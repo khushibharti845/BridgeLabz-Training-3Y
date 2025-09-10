a4d1e1dcbdfb207890f66263cd6498ff8cec7a4b
Your Name <khushibharti845@gmail.com>
2025-09-10 14:45:06 +0530

    added

---
+++ Methods/Level-2/BMICalculator.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class BMICalculator {
+
+    public static void calculateBMI(double[][] data) {
+        for (int i = 0; i < data.length; i++) {
+            double weight = data[i][0];
+            double heightCm = data[i][1];
+            double heightM = heightCm / 100;
+            double bmi = weight / (heightM * heightM);
+            data[i][2] = bmi;
+        }
+    }
+
+    public static String[] getBMIStatus(double[][] data) {
+        String[] statuses = new String[data.length];
+        for (int i = 0; i < data.length; i++) {
+            double bmi = data[i][2];
+            if (bmi < 18.5) {
+                statuses[i] = "Underweight";
+            } else if (bmi < 25) {
+                statuses[i] = "Normal";
+            } else if (bmi < 30) {
+                statuses[i] = "Overweight";
+            } else {
+                statuses[i] = "Obese";
+            }
+        }
+        return statuses;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[][] data = new double[10][3];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter weight (kg) for person " + (i + 1) + ": ");
+            data[i][0] = sc.nextDouble();
+            System.out.print("Enter height (cm) for person " + (i + 1) + ": ");
+            data[i][1] = sc.nextDouble();
+        }
+
+        calculateBMI(data);
+        String[] statuses = getBMIStatus(data);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("Person %d -> Weight: %.2f kg, Height: %.2f cm, BMI: %.2f, Status: %s%n",
+                    i + 1, data[i][0], data[i][1], data[i][2], statuses[i]);
+        }
+    }
+}
